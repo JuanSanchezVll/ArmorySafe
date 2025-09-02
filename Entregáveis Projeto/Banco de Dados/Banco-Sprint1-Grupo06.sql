7302ba79bfdd83aca267e2beff581f3a9c6a34e7
D-Foschini <daniel.foschini@sptech.school>
2025-09-02 18:54:32 -0300

    Atualização do banco de dados para segunda Versão

--- Entregáveis Projeto/Banco de Dados/Banco-Sprint1-Grupo06.sql
+++ Entregáveis Projeto/Banco de Dados/Banco-Sprint1-Grupo06.sql
@@ -17,29 +17,29 @@ CREATE DATABASE armorysafe; -- CRIA O BANCO DE DADOS
 USE armorysafe; -- SELECIONA O BANCO DA DADOS
 
 CREATE TABLE cadastro( -- CRIA A TABELA DO CADASTRO DE MILITARES
-idLog INT PRIMARY KEY AUTO_INCREMENT, -- PKEY
+idLogin INT PRIMARY KEY AUTO_INCREMENT, -- PKEY
 cim CHAR(10) UNIQUE NOT NULL, -- Nº INTENTIDADE MILITAR (CIM)
 senha VARCHAR(30) NOT NULL, -- SENHA DE LOGIN
-dtCria DATETIME DEFAULT CURRENT_TIMESTAMP, -- DATA QUE FOI REALIZADO O CADASTRO
-tipUsr VARCHAR(30) NOT NULL -- TIPO DE USUARIO
-CONSTRAINT chk_hirar CHECK(tipUsr IN('Praça','Subalterno','Superior')),
-orgMil VARCHAR(40) NOT NULL, -- ORGANIZAÇÃO MILITAR QUE É O MILITAR
-cmdMili VARCHAR(30) NOT NULL, -- COMANDOS MILITARES DO USUARIO (FAZER CONSTRAINT)	
+dtCriacao DATETIME DEFAULT CURRENT_TIMESTAMP, -- DATA QUE FOI REALIZADO O CADASTRO
+tipUsuario VARCHAR(30) NOT NULL -- TIPO DE USUARIO
+CONSTRAINT chk_hirar CHECK(tipUsuario IN('Praça','Subalterno','Superior')),
+organizacao VARCHAR(40) NOT NULL, -- ORGANIZAÇÃO MILITAR QUE É O MILITAR
+comando VARCHAR(30) NOT NULL, -- COMANDOS MILITARES DO USUARIO (FAZER CONSTRAINT)	
 sexo CHAR(1), -- SEXO DO USUARIO
 CONSTRAINT chk_sex CHECK(sexo IN('F','M'))
 );
 
 CREATE TABLE lote( -- CRIA A TABELA DO PAIOL
-idMun INT PRIMARY KEY AUTO_INCREMENT, -- PKEY IND
-idLote VARCHAR(10) NOT NULL UNIQUE, -- QUAL O LOTE DA MUNIÇÃO
+idMunicao INT PRIMARY KEY AUTO_INCREMENT, -- PKEY IND
+numLote VARCHAR(10) NOT NULL UNIQUE, -- QUAL O LOTE DA MUNIÇÃO
 qtdCaixa INT, -- QUANTIA DE CAIXAS PRESENTES NO LOTE
-localiza VARCHAR(40) NOT NULL DEFAULT'Não Designado', -- EM QUAL QUARTEL A MUNIÇAO ESTÁ LOCALIZADA
+organizacao VARCHAR(40) NOT NULL DEFAULT'Não Designado', -- EM QUAL QUARTEL A MUNIÇAO ESTÁ LOCALIZADA
 comando varchar(30) NOT NULL, -- EM QUAL COMANDO MILITAR É LOCALIZADO O QUARTEL!
 paiol VARCHAR(40) NOT NULL, -- EM QUAL PAIOL DA OM A MUNIÇÃO ESTÁ LOCALIZADA!
 calibre VARCHAR(10) NOT NULL, -- TIPO/CLASSIFICAÇÃO DA MUNIÇÃO
-dtRec DATETIME, -- DATA DE RECEBIMENTO
-dtFab DATE, -- DATA DE FABRICAÇÃO
-dtExm DATETIME, -- DATA DO ULTIMO EXAME REALIZADO
+dtRecebimento DATETIME, -- DATA DE RECEBIMENTO
+dtFabricacao DATE, -- DATA DE FABRICAÇÃO
+dtExame DATETIME, -- DATA DO ULTIMO EXAME REALIZADO
 marcacao VARCHAR(11) DEFAULT 'TESTED' -- QUAIS TRABALHOS FORAM FEITOS NA MUNIÇÃO
 CONSTRAINT chk_mark CHECK( marcacao IN('A','R','REP','INSP','PKD','DES','COND','US/T','TESTED','FAILED TEST')),
 condicao CHAR(2) DEFAULT'A1' -- CONDIÇÃO ATUAL DA MUNIÇÃO
@@ -47,12 +47,12 @@ CONSTRAINT chk_cond CHECK( condicao IN('A1','A2','A3','B1','B2','B3','B4','C1','
 );
 
 CREATE TABLE umidade(
-idUm INT PRIMARY KEY AUTO_INCREMENT, -- INDENTIFICAÇÃO DO SENSOR
-dtCap DATETIME, -- A DATA & HORARIO DA CAPTURA
-qtUm DECIMAL(4,2),  -- 12.34% UMIDADE CAPTURADA
-locSen VARCHAR(40), -- EM QUAL OM O SENSOR ESTÁ LOCALIZADO
-paiSen VARCHAR(40), -- EM QUAL PAIOL DA OM O SENSOR ESTÁ LOCALIZADO
-comSen VARCHAR(30) -- EM QUAL COMANDO MILITAR ESTÁ LOCALIZADO A OM DO SENSOR.
+idUmimdade INT PRIMARY KEY AUTO_INCREMENT, -- INDENTIFICAÇÃO DO SENSOR
+dtCaptura DATETIME, -- A DATA & HORARIO DA CAPTURA
+qtUmidade DECIMAL(4,2),  -- 12.34% UMIDADE CAPTURADA
+organizacao VARCHAR(40), -- EM QUAL OM O SENSOR ESTÁ LOCALIZADO
+paiol VARCHAR(40), -- EM QUAL PAIOL DA OM O SENSOR ESTÁ LOCALIZADO
+comando VARCHAR(30) -- EM QUAL COMANDO MILITAR ESTÁ LOCALIZADO A OM DO SENSOR.
 );
 
 DESC umidade;
@@ -61,7 +61,7 @@ DESC municao;
 
 -- INSERINDO DADOS --
 
-INSERT INTO cadastro (cim, senha, tipUsr, orgMil, cmdMili, sexo)
+INSERT INTO cadastro (cim, senha, tipUsuario, organizacao, comando, sexo)
 VALUES
 ('9283746150', 'senha123', 'Praça', '12º Batalhão de Infantaria', 'CMSE', 'M'),
 ('4819273650', 'forca2025', 'Praça', '4ª Companhia de Comunicações', 'CMSE', 'F'),
@@ -76,8 +76,7 @@ VALUES
 
 SELECT * FROM cadastro;
 
-
-INSERT INTO lote (idLote, qtdCaixa, localiza, comando, paiol, calibre, dtRec, dtFab, dtExm, marcacao, condicao)
+INSERT INTO lote (numLote, qtdCaixa, organizacao, comando, paiol, calibre, dtRecebimento, dtFabricacao, dtExame, marcacao, condicao)
 VALUES
 ('L2025A001', 5, '12º Batalhão de Infantaria', 'CMSE', 'Paiol Central', '7.62mm', '2025-01-10 09:30:00', '2023-05-12', '2025-01-20 14:00:00', 'TESTED', 'A1'),
 ('L2025A002', 12, '4ª Companhia de Comunicações', 'CMSE', 'Paiol 1', '5.56mm', '2025-02-05 11:00:00', '2022-11-20', '2025-02-15 08:00:00', 'US/T', 'A2'),
@@ -92,7 +91,7 @@ VALUES
 
 SELECT * FROM lote;
 
-INSERT INTO umidade (dtCap, qtUm, locSen, paiSen, comSen)
+INSERT INTO umidade (dtCaptura, qtUmidade, organizacao, paiol, comando)
 VALUES
 ('2025-04-02 09:15:00', 45.32, '12º Batalhão de Infantaria', 'Paiol Central', 'CMSE'),
 ('2025-04-02 09:30:00', 50.87, '4ª Companhia de Comunicações', 'Paiol Alfa', 'CMSE'),
@@ -109,27 +108,27 @@ SELECT * FROM umidade;
 
 -- REALIZANDO SELECT --
 
-SELECT * FROM cadastro WHERE tipUsr ='Praça';
+SELECT * FROM cadastro WHERE tipUsuario ='Praça';
 
 SELECT * FROM cadastro WHERE sexo ='M';
 
-SELECT * FROM cadastro WHERE tipUsr !='Superior';
+SELECT * FROM cadastro WHERE tipUsuario !='Superior';
 
-SELECT dtCria,cim FROM cadastro WHERE tipUsr ='Subalterno';
+SELECT dtCriacao,cim FROM cadastro WHERE tipUsuario ='Subalterno';
 
 SELECT 
 	cim AS 'Nº INDENTIDADE',
-	dtCria AS 'DATA DE CRIAÇÃO',
-	tipUsr AS 'PATENTE',
-	orgMil AS 'ORGANIZAÇÃO MILITAR',
-	cmdMili AS 'COMANDO MILITAR'
+	dtCriacao AS 'DATA DE CRIAÇÃO',
+	tipUsuario AS 'PATENTE',
+	organizacao AS 'ORGANIZAÇÃO MILITAR',
+	comando AS 'COMANDO MILITAR'
 FROM cadastro;
 
 SELECT cim AS 'INTENTIDADE',
     CASE
-	WHEN tipUsr = 'Praça' THEN 'Baixa patente'
-	WHEN tipUsr = 'Subalterno' THEN 'Média patente'
-	WHEN tipUsr = 'Superior' THEN 'Alta patente'
+	WHEN tipUsuario = 'Praça' THEN 'Baixa patente'
+	WHEN tipUsuario = 'Subalterno' THEN 'Média patente'
+	WHEN tipUsuario = 'Superior' THEN 'Alta patente'
 	ELSE 'Desconhecido'
     END AS CLASSIFICAÇAO
 FROM cadastro;
@@ -138,52 +137,73 @@ SELECT * FROM lote WHERE qtdCaixa >10;
 
 SELECT * FROM lote WHERE calibre ='105mm';
 
-UPDATE lote SET marcacao ='INSP' WHERE idMun =9;
+UPDATE lote SET marcacao ='INSP' WHERE idMunicao =9;
 
 SELECT * FROM lote WHERE comando ='CMSE';
 
 SELECT
-	idLote AS 'Nº DO LOTE',
+	numLote AS 'Nº DO LOTE',
 	qtdCaixa AS 'QUANTIA DE CAIXAS',
-	localiza AS 'ORGANIZAÇÃO MILITAR',
+	organizacao AS 'ORGANIZAÇÃO MILITAR',
 	comando AS 'COMANDO MILITAR',
 	calibre AS 'MILIMETRAGEM',
-	dtRec AS 'DATA DE RECEBIMENTO'
+	dtRecebimento AS 'DATA DE RECEBIMENTO'
 FROM lote WHERE
 marcacao = 'TESTED';
 
-SELECT idLote AS 'LOTE',
+SELECT numLote AS 'LOTE',
 	CASE
-	WHEN marcacao !='TESTED' THEN 'NÃO PRONTA'
-    ELSE 'EM PRONTIDÃO'
+	WHEN marcacao !='TESTED' THEN 'Não Pronta'
+    ELSE 'Em Prontidão'
     END AS SITUAÇÃO
 FROM lote WHERE comando = 'CMSE';
     
     
-SELECT * FROM umidade WHERE qtum >80;
+SELECT * FROM umidade WHERE qtUmidade >80;
 
-SELECT * FROM umidade WHERE locSEN LIKE '%6%';
+SELECT * FROM umidade WHERE organizacao LIKE '%6%';
 
-SELECT * FROM umidade WHERE dtCap LIKE'%9:15%';
+SELECT * FROM umidade WHERE dtCaptura LIKE'%9:15%';
 
 SELECT
-	dtCap AS 'DATA DE CAPTURA',
-	qtUm AS '% DE UMIDADE',
-	locSen AS 'ORGANIZAÇÃO MILITAR',
-	paiSen AS 'PAIOL'
-FROM umidade WHERE qtUm >49;
+	dtCaptura AS 'DATA DE CAPTURA',
+	qtUmidade AS '% DE UMIDADE',
+	organizacao AS 'ORGANIZAÇÃO MILITAR',
+	paiol AS 'PAIOL'
+FROM umidade WHERE qtUmidade >49;
 
-SELECT locSen AS 'ORGANIZAÇÃO MILITAR', paiSen AS 'PAIOL',
+SELECT organizacao AS 'ORGANIZAÇÃO MILITAR', paiol AS 'PAIOL',
 	CASE
-    WHEN qtUm <40 THEN 'Umidade Baixa'
-    WHEN qtUm >60 THEN 'Umidade Alta'
+    WHEN qtUmidade <40 THEN 'Umidade Baixa'
+    WHEN qtUmidade >60 THEN 'Umidade Alta'
     ELSE 'Umidade Ideal'
 	END AS SITUAÇÃO
 FROM umidade;
 
--- ANOTAÇÃO
 
-/* 
+/* ANOTAÇÕES
+ -- A FAZERES --
+ 
+ PADRONIZAR NOME DAS TABLES (feito)
+ 
+ 
+ --IDEIAS PARA O FUTURO--
+ 
+CRIAR TABELA DE AUDITORIA (futuramente)
 
-CRIAR TABELA DE AUDITORIA
-PADRONIZAR NOME DAS TABLES
+CRIAR TABELAS DE ID (OM, COM) PARA FACILITAR RELACIONAR AS TABLES (futuramente)
+*/
+
+/* EXEMPLOS DE FUTURAS TABLES
+
+CREATE TABLE comandomilitar(
+numComando INT PRIMARY KEY,
+descricao VARCHAR(100)
+);
+
+CREATE TABLE organizacao(
+numOrganizacao INT PRIMARY KEY,
+descricao VARCHAR(100)
+);
+
+*/
